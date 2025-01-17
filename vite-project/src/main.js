598be9b5bbdb67bfae8620d22e8fabcbc1d588bc
ArinaHolodkova <arina.holodkova@student.howest.be>
2025-01-17 15:43:57 +0100

    navigation hamburger

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.addEventListener("DOMContentLoaded", function () {
+  const navButton = document.querySelector(".nav__button");
+  const navList = document.querySelector(".nav__list");
+  const iconLink = document.getElementById("iconlink");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  navButton.addEventListener("click", function () {
+    const expanded = navButton.getAttribute("aria-expanded") === "true";
+    navButton.setAttribute("aria-expanded", !expanded);
 
-setupCounter(document.querySelector('#counter'))
+    iconLink.setAttribute("xlink:href", expanded ? "#navicon" : "#close");
+    navList.style.display = expanded ? "none" : "flex";
+  });
+});
