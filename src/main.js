0302cb04b45161ee1f996c1a9d2b7dae679a6a6c
ArinaHolodkova <arina.holodkova@student.howest.be>
2025-01-23 14:20:32 +0100

    question part hidden , courusel and swipe in html and not js

--- src/main.js
+++ src/main.js
@@ -3,58 +3,22 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
+// import golobal_c_scale from "./assets/global_understanding/golobal_c_scale,w_200.avif";
+// import greece_w_200 from "./assets/greece/greece,w_200.avif";
+
 gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
 
-const imageElement = document.getElementById("dynamicImage");
-const textElement = document.getElementById("dynamicText");
-let startX = 0;
-let endX = 0;
+const textElement = document.querySelector("#dynamicText");
+const images = document.querySelectorAll(".intro__img");
 let currentSwipe = 0;
-  // const firstShoe = document.querySelector(".first-shoe");
-  // const secondShoe = document.querySelector(".second-shoe");
-  // let isStepping = false;
-  // let startY = 0;
+
+const carouselItems = document.querySelectorAll(".carousel__wrapper__item");
+const prevButton = document.querySelector(".carousel__prev");
+const nextButton = document.querySelector(".carousel__next");
+let currentIndex = 0;
 
 
-const images = [
-  {
-    srcset: `
-      ./src/assets/global_understanding/golobal_c_scale,w_200.avif 200w,
-      ./src/assets/global_understanding/golobal_c_scale,w_534.avif 534w,
-      ./src/assets/global_understanding/golobal_c_scale,w_796.avif 796w,
-      ./src/assets/global_understanding/golobal_c_scale,w_1047.avif 1047w,
-      ./src/assets/global_understanding/golobal_c_scale,w_1080.avif 1080w
-    `,
-    sizes: "(max-width: 1080px) 100vw, 796px",
-    src: "./src/assets/global_understanding/golobal.png",
-    text: "Global understanding",
-  },
-  {
-    srcset: `
-      ./src/assets/science/science,w_200.avif 200w,
-      ./src/assets/science/science,w_515.avif 515w,
-      ./src/assets/science/science,w_824.avif 824w,
-      ./src/assets/science/science,w_1080.avif 1080w
-    `,
-    sizes: "(max-width: 1080px) 100vw, 515px",
-    src: "./src/assets/science/science.png",
-    text: "Science",
-  },
-  {
-    srcset: `
-      ./src/assets/art/art,w_200.avif 200w,
-      ./src/assets/art/art,w_514.avif 514w,
-      ./src/assets/art/art,w_739.avif 739w,
-      ./src/assets/art/art,w_1018.avif 1018w,
-      ./src/assets/art/art,w_1080.avif 1080w
-    `,
-    sizes: "(max-width: 1080px) 100vw, 739px",
-    src: "./src/assets/art/art.png",
-    text: "Art",
-  },
-];
-
-const navigation= ()=> {
+const navigation = () => {
   const navButton = document.querySelector(".nav__button");
   const navList = document.querySelector(".nav__list");
   const iconLink = document.getElementById("iconlink");
@@ -68,164 +32,122 @@ const navigation= ()=> {
   });
 };
 
-const carouselEffect = () => {
-  const carouselimages = [
-    {
-      srcset: `
-./src/assets/greece/greece,w_200.avif 200w,
-./src/assets/greece/greece,w_266.avif 266w,
-./src/assets/greece/greece,w_324.avif 324w,
-./src/assets/greece/greece,w_374.avif 374w,
-./src/assets/greece/greece,w_425.avif 425w,
-./src/assets/greece/greece,w_476.avif 476w,
-./src/assets/greece/greece,w_524.avif 524w,
-./src/assets/greece/greece,w_573.avif 573w,
-./src/assets/greece/greece,w_617.avif 617w,
-./src/assets/greece/greece,w_662.avif 662w,
-./src/assets/greece/greece,w_706.avif 706w,
-./src/assets/greece/greece,w_750.avif 750w,
-./src/assets/greece/greece,w_1080.avif 1080w
-    `,
-      sizes: "(max-width: 1080px) 100vw, 1080px",
-      src: "./src/assets/greece/greece.png",
-      alt: "Greek language",
-    },
-    {
-      srcset: `
-./src/assets/hebrew/hebrew,w_200.avif 200w,
-./src/assets/hebrew/hebrew,w_270.avif 270w,
-./src/assets/hebrew/hebrew,w_332.avif 332w,
-./src/assets/hebrew/hebrew,w_384.avif 384w,
-./src/assets/hebrew/hebrew,w_440.avif 440w,
-./src/assets/hebrew/hebrew,w_493.avif 493w,
-./src/assets/hebrew/hebrew,w_541.avif 541w,
-./src/assets/hebrew/hebrew,w_588.avif 588w,
-./src/assets/hebrew/hebrew,w_632.avif 632w,
-./src/assets/hebrew/hebrew,w_674.avif 674w,
-./src/assets/hebrew/hebrew,w_717.avif 717w,
-./src/assets/hebrew/hebrew,w_754.avif 754w,
-./src/assets/hebrew/hebrew,w_1080.avif 1080w
-    `,
-      sizes: "(max-width: 1080px) 100vw, 1080px",
-      src: "./src/assets/hebrew/hebrew.png",
-      alt: "Hebrew language",
-    },
-    {
-      srcset: `
-./src/assets/latin/latin,w_200.avif 200w,
-./src/assets/latin/latin,w_257.avif 257w,
-./src/assets/latin/latin,w_307.avif 307w,
-./src/assets/latin/latin,w_348.avif 348w,
-./src/assets/latin/latin,w_393.avif 393w,
-./src/assets/latin/latin,w_433.avif 433w,
-./src/assets/latin/latin,w_471.avif 471w,
-./src/assets/latin/latin,w_510.avif 510w,
-./src/assets/latin/latin,w_546.avif 546w,
-./src/assets/latin/latin,w_583.avif 583w,
-./src/assets/latin/latin,w_620.avif 620w,
-./src/assets/latin/latin,w_656.avif 656w,
-./src/assets/latin/latin,w_691.avif 691w,
-./src/assets/latin/latin,w_725.avif 725w,
-./src/assets/latin/latin,w_759.avif 759w,
-./src/assets/latin/latin,w_795.avif 795w,
-./src/assets/latin/latin,w_829.avif 829w,
-./src/assets/latin/latin,w_1080.avif 1080w
-    `,
-      sizes: "(max-width: 1080px) 100vw, 1080px",
-      src: "./src/assets/latin/latin.png",
-      alt: "Latin language",
-    },
-  ];
 
-  let currentIndex = 0;
-
-  const imageContainer = document.querySelector(".carousel__wrapper");
-  const prevButton = document.querySelector(".carousel__prev");
-  const nextButton = document.querySelector(".carousel__next");
-
-  const updateImage = () => {
-    const { sizes, srcset, src, alt } = carouselimages[currentIndex];
-    imageContainer.innerHTML = `
-      <img sizes="${sizes}" srcset="${srcset}" src="${src}" alt="${alt}" class="carousel__image">
-    `;
-  };
-
-  const changeImage = (direction) => {
-    currentIndex = (currentIndex + direction + images.length) % carouselimages.length;
-    updateImage();
-  };
-
-  prevButton.addEventListener("click", () => changeImage(-1));
-  nextButton.addEventListener("click", () => changeImage(+1));
-
-
-  updateImage();
+const updateCarousel = () => {
+  carouselItems.forEach((item, index) => {
+    if (index === currentIndex) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
 };
+const carouselEffect =()=>{
+prevButton.addEventListener("click", () => {
+  currentIndex =
+    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  updateCarousel();
+});
 
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  updateCarousel();
+});
+}
 
 const questionSection = () => {
   const questionContainer = document.querySelector(".question__section");
   const carouselSection = document.querySelector(".bible__languages__carousel");
+  const bibleTitle = document.querySelector(".bible__languages__comment");
+    const answer1 = document.querySelector("#answer__1");
+  const answer2 = document.querySelector("#answer__2");
   const answer3 = document.querySelector("#answer__3");
+    const answer4 = document.querySelector("#answer__4");
   const answer5 = document.querySelector("#answer__5");
-const questionText = document.querySelector(".question__text");
+  const questionText = document.querySelector(".question__text");
 
- const revealCarousel = () => {
-    questionText.style.transition = "transform 0.5s ease-in-out"; 
-   
+const revealCarousel = () => {
+  questionText.style.transition = "transform 0.5s ease-in-out";
+
+  setTimeout(() => {
+    questionContainer.style.opacity = "0";
     setTimeout(() => {
-      questionContainer.style.opacity = "0";
-      setTimeout(() => {
-        questionContainer.style.display = "none";
-        carouselSection.classList.add("visible"); 
-      }, 500);
-    }, 500); 
-  };
+      questionContainer.style.display = "none";
 
-  
+      carouselSection.style.display = "block"; 
+      carouselSection.style.opacity = "1"; 
+      carouselSection.style.transition = "opacity 0.5s ease-in-out";
+    }, 500);
+  }, 500);
+};
+  answer1.addEventListener("click", () => {
+    revealCarousel();
+    bibleTitle.textContent = " Not even close";
+    questionText.style.transform = "translateX(-70%)";
+  });
+  answer2.addEventListener("click", () => {
+    revealCarousel();
+    bibleTitle.textContent =" Not even close" ;
+    questionText.style.transform = "translateX(-70%)";
+  });
   answer3.addEventListener("click", () => {
     revealCarousel();
-     questionText.style.transform = "translateY(-70%)";
+     bibleTitle.textContent = " Not even close";
+    questionText.style.transform = "translateX(-70%)";
   });
-
+    answer4.addEventListener("click", () => {
+      revealCarousel();
+      bibleTitle.textContent = "Almost there!";
+      questionText.style.transform = "translateX(70%)";
+    });
   answer5.addEventListener("click", () => {
     revealCarousel();
-     questionText.style.transform = "translateY(70%)";
+    bibleTitle.textContent =
+      "Were you involved in the creation process? Because this is the correct answer ";
+    questionText.style.transform = "translateY(70%)";
   });
 };
 
 
-
-
 const swipeEffect = () => {
+  let startX = 0;
+  let endX = 0;
+
+  
   document.addEventListener("touchstart", (event) => {
     startX = event.touches[0].clientX;
   });
 
+  
   document.addEventListener("touchmove", (event) => {
     endX = event.touches[0].clientX;
   });
 
+
   document.addEventListener("touchend", () => {
     if (startX > endX + 90) {
-   
-      currentSwipe = (currentSwipe + 1) % images.length;
+      currentSwipe = (currentSwipe + 1) % images.length; 
       updateContent();
     } else if (startX < endX - 50) {
-    
-      currentSwipe = (currentSwipe - 1 + images.length) % images.length;
+      currentSwipe = (currentSwipe - 1 + images.length) % images.length; 
       updateContent();
     }
   });
 };
 
 const updateContent = () => {
-  const currentImage = images[currentSwipe];
-  imageElement.srcset = currentImage.srcset;
-  imageElement.sizes = currentImage.sizes;
-  imageElement.src = currentImage.src;
-  textElement.textContent = currentImage.text;
-    updateDots();
+  images.forEach((image, index) => {
+    if (index === currentSwipe) {
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+
+  const texts = ["Global understanding", "Art", "Science"];
+  textElement.textContent = texts[currentSwipe];
+
+  updateDots();
 };
 
 const updateDots = () => {
@@ -240,11 +162,13 @@ const updateDots = () => {
 };
 
 
+
 const createDots = () => {
   const dotsContainer = document.querySelector(".intro__dots");
   images.forEach((_, index) => {
     const dot = document.createElement("div");
     dot.classList.add("intro__dot");
+    if (index === 0) dot.classList.add("active"); // First dot is active by default
     dot.addEventListener("click", () => {
       currentSwipe = index;
       updateContent();
@@ -254,9 +178,9 @@ const createDots = () => {
 };
 
 
-
 const animatePath = (pathSelector, triggerSelector) => {
-  const pathElement = document.querySelector(pathSelector)
+  const pathElement = document
+    .querySelector(pathSelector)
     .querySelector("path");
   const pathLength = pathElement.getTotalLength();
 
@@ -267,12 +191,12 @@ const animatePath = (pathSelector, triggerSelector) => {
     { strokeDashoffset: pathLength - 5 },
     {
       strokeDashoffset: 0,
-      duration: 1,
+      duration: 3,
       scrollTrigger: {
         trigger: triggerSelector,
         start: "top top",
-        markers:true,
-        scrub: true,
+        markers: true,
+        // scrub: true,
       },
     }
   );
@@ -288,14 +212,13 @@ const animatePath = (pathSelector, triggerSelector) => {
         start: "middle 5%",
         ease: "power2.inOut",
         scrub: true,
-  
       },
       alpha: 1,
     }
   );
 };
 
-const leafPop =()=>{
+const leafPop = () => {
   document.querySelectorAll(".leaf").forEach((leaf) => {
     leaf.addEventListener("click", () => {
       leaf.classList.add("popped");
@@ -306,64 +229,61 @@ const leafPop =()=>{
   });
 };
 
+const triggerChange = () => {
+  console.log("Changing images");
+  const images = document.querySelectorAll(".end__combine__image");
+  const bigImage = document.querySelector(".big-image");
 
-  const triggerChange=() =>{
-    console.log("Changing images"); 
-    const images = document.querySelectorAll(".end__combine__image");
-    const bigImage = document.querySelector(".big-image");
+  images.forEach((img) => {
+    img.style.opacity = "0";
+    img.style.transform = "scale(0.8)";
+  });
 
-    images.forEach((img) => {
-      img.style.opacity = "0";
-      img.style.transform = "scale(0.8)";
-    });
+  setTimeout(() => {
+    images.forEach((img) => img.classList.add("hidden"));
+    bigImage.style.opacity = "1";
+    bigImage.classList.remove("hidden");
+  }, 500);
+};
 
-    setTimeout(() => {
-      images.forEach((img) => img.classList.add("hidden"));
-      bigImage.style.opacity = "1";
-      bigImage.classList.remove("hidden");
-    }, 500);
-    
+const shakeIt = () => {
+  console.log("Script loaded");
+
+  const simulateShakeButton = document.getElementById("simulate-shake");
+  if (simulateShakeButton) {
+    simulateShakeButton.addEventListener("click", triggerChange);
+  } else {
+    console.log("Simulate shake button not found");
   }
 
-  const shakeIt =() => {
-    console.log("Script loaded"); 
+  let shakeThreshold = 15;
 
-     const simulateShakeButton = document.getElementById("simulate-shake");
-     if (simulateShakeButton) {
-       simulateShakeButton.addEventListener("click", triggerChange);
-     } else {
-       console.log("Simulate shake button not found");
-     }
+  console.log("DeviceMotionEvent supported");
+  let lastX = 0,
+    lastY = 0,
+    lastZ = 0;
 
-    let shakeThreshold = 15; 
+  const shakeEvent = (event) => {
+    const { x, y, z } = event.accelerationIncludingGravity || {};
 
-      console.log("DeviceMotionEvent supported"); 
-      let lastX = 0,
-        lastY = 0,
-        lastZ = 0;
+    if (!x || !y || !z) return;
 
-      const shakeEvent = (event) => {
-        const { x, y, z } = event.accelerationIncludingGravity || {};
+    const deltaX = Math.abs(lastX - x);
+    const deltaY = Math.abs(lastY - y);
+    const deltaZ = Math.abs(lastZ - z);
 
-        if (!x || !y || !z) return;
+    if (deltaX + deltaY + deltaZ > shakeThreshold) {
+      console.log("Triggering change");
+      triggerChange();
+    }
 
-        const deltaX = Math.abs(lastX - x);
-        const deltaY = Math.abs(lastY - y);
-        const deltaZ = Math.abs(lastZ - z);
-
-        if (deltaX + deltaY + deltaZ > shakeThreshold) {
-          console.log("Triggering change"); 
-          triggerChange();
-        }
-
-        lastX = x;
-        lastY = y;
-        lastZ = z;
-      };
-
-      window.addEventListener("devicemotion", shakeEvent, false);
-  
+    lastX = x;
+    lastY = y;
+    lastZ = z;
   };
+
+  window.addEventListener("devicemotion", shakeEvent, false);
+};
 
 const canvas = document.querySelector("#puzzleCanvas");
 const ctx = canvas.getContext("2d");
@@ -376,15 +296,11 @@ const getResponsiveImageSource = () => {
     { src: "./src/assets/puzzle/puzzle,w_660.avif", maxWidth: 660 },
   ];
 
-
   const srcsetString = srcset
     .map((item) => `${item.src} ${item.maxWidth}w`)
     .join(", ");
 
-
-  
   const sizes = "(max-width: 1080px) 100vw, 796px";
-
 
   return {
     srcset: srcsetString,
@@ -409,7 +325,6 @@ const pieceHeight = canvas.height / rows;
 let pieces = [];
 let draggingPiece = null;
 let offsetX, offsetY;
-
 
 const createPiece = () => {
   for (let row = 0; row < rows; row++) {
@@ -462,96 +377,91 @@ const getClickedPiece = (x, y) => {
   return clickedPiece;
 };
 
-const mouseEvents =()=>{
+const mouseEvents = () => {
+  canvas.addEventListener("mousedown", (e) => {
+    console.log("mousedown");
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-canvas.addEventListener("mousedown", (e) => {
-  console.log("mousedown");
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+    draggingPiece = getClickedPiece(x, y);
 
-  draggingPiece = getClickedPiece(x, y);
-
-  if (draggingPiece) {
-    offsetX = x - draggingPiece.x;
-    offsetY = y - draggingPiece.y;
-    console.log("Started dragging piece:", draggingPiece);
-  }
-});
-
-
-canvas.addEventListener("mousemove", (e) => {
-  if (!draggingPiece) return;
-  console.log("mousemove");
-
-  const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  draggingPiece.x = x - offsetX;
-  draggingPiece.y = y - offsetY;
-
-  drawPieces();
-});
-
-
-canvas.addEventListener("mouseup", () => {
-  if (!draggingPiece) return;
-  console.log("mouseup");
-
-  const snapX = Math.round(draggingPiece.x / pieceWidth) * pieceWidth;
-  const snapY = Math.round(draggingPiece.y / pieceHeight) * pieceHeight;
-
-  if (
-    Math.abs(snapX - draggingPiece.x) < 10 &&
-    Math.abs(snapY - draggingPiece.y) < 10
-  ) {
-    draggingPiece.x = snapX;
-    draggingPiece.y = snapY;
-  }
-
-  draggingPiece = null;
-  drawPieces();
-});
-
-imgPuzzle.onload = () => {
-  console.log("Image loaded");
-  createPiece();
-  drawPieces();
-};
-}
-
-const generation =()=> {
-const pictures = document.querySelectorAll(
-  ".botanic__illustrations__generations picture"
-);
-
-pictures.forEach((picture) => {
-  gsap.fromTo(
-    picture,
-    { scale: 0.7, opacity: 0.5 },
-
-    {
-      scale: 1.2,
-      opacity: 1,
-      scrollTrigger: {
-        trigger: picture,
-        start: "center 75%",
-        end: "center 25%",
-        scrub: true,
-      },
+    if (draggingPiece) {
+      offsetX = x - draggingPiece.x;
+      offsetY = y - draggingPiece.y;
+      console.log("Started dragging piece:", draggingPiece);
     }
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (!draggingPiece) return;
+    console.log("mousemove");
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    draggingPiece.x = x - offsetX;
+    draggingPiece.y = y - offsetY;
+
+    drawPieces();
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    if (!draggingPiece) return;
+    console.log("mouseup");
+
+    const snapX = Math.round(draggingPiece.x / pieceWidth) * pieceWidth;
+    const snapY = Math.round(draggingPiece.y / pieceHeight) * pieceHeight;
+
+    if (
+      Math.abs(snapX - draggingPiece.x) < 10 &&
+      Math.abs(snapY - draggingPiece.y) < 10
+    ) {
+      draggingPiece.x = snapX;
+      draggingPiece.y = snapY;
+    }
+
+    draggingPiece = null;
+    drawPieces();
+  });
+
+  imgPuzzle.onload = () => {
+    console.log("Image loaded");
+    createPiece();
+    drawPieces();
+  };
+};
+
+const generation = () => {
+  const pictures = document.querySelectorAll(
+    ".botanic__illustrations__generations picture"
   );
-});
-}
 
+  pictures.forEach((picture) => {
+    gsap.fromTo(
+      picture,
+      { scale: 0.7, opacity: 0.5 },
 
+      {
+        scale: 1.2,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: picture,
+          start: "center 75%",
+          end: "center 25%",
+          scrub: true,
+        },
+      }
+    );
+  });
+};
 
 const init = () => {
   questionSection();
   createDots();
   updateContent();
-generation();
+  generation();
   mouseEvents();
   navigation();
   leafPop();
@@ -560,6 +470,5 @@ generation();
   swipeEffect();
   animatePath("#wavyLineOne", ".bible__problems__one");
   animatePath("#wavyLineTwo", ".bible__problems__two");
-
 };
 init();
